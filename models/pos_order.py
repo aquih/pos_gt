63105a3982eeac71e88e71706d1270675dbd5b8e
Rodrigo Fernandez <rodrigo@aquih.com>
2019-04-05 20:57:15 -0600

    Campo correcto

--- models/pos_order.py
+++ models/pos_order.py
@@ -46,7 +46,7 @@ class PosOrder(models.Model):
                     'journal': p.journal_id.id,
                 })
             nueva.action_pos_order_invoice()
-            nueva.invoice_id.numero_viejo = self.name
+            nueva.invoice_id.numero_viejo = self.invoice_id.name
             logging.warn(nueva.invoice_id.numero_viejo)
             nueva.invoice_id.sudo().action_invoice_open()
             nueva.account_move = nueva.invoice_id.move_id
