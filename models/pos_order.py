96e93e8a6f82c7ad1fe7a0d4bfbdf47c0f6686b1
Rodrigo Fernández <rodrigo@aquih.com>
2023-09-04 18:44:47 +0000

    Cualquiera pueda registrar movimientos de dinero y buscar todos los pedidos en todos los puntos de ventas

--- models/pos_order.py
+++ models/pos_order.py
@@ -2,6 +2,7 @@
 
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
+from odoo.osv.expression import AND
 import logging
 
 class PosOrder(models.Model):
@@ -26,6 +27,16 @@ class PosOrder(models.Model):
     def _create_order_picking(self):
         self = self.with_context(analytic_account_id=self.config_id.analytic_account_id)
         super(PosOrder, self)._create_order_picking()
+        
+            
+    @api.model
+    def search_paid_order_ids(self, config_id, domain, limit, offset):
+        """Search for 'paid' orders that satisfy the given domain, limit and offset."""
+        default_domain = ['!', '|', ('state', '=', 'draft'), ('state', '=', 'cancelled')]
+        real_domain = AND([domain, default_domain])
+        ids = self.search(AND([domain, default_domain]), limit=limit, offset=offset).ids
+        totalCount = self.search_count(real_domain)
+        return {'ids': ids, 'totalCount': totalCount}
         
     def refund(self):
         res = super(PosOrder, self).refund()
