dd6185556b845aeb21ed96d85c7f049371f78f89
rocketgithub <rodrigo@aquih.com>
2024-03-14 10:47:45 -0600

    Buscar todos los pedidos en todos los puntos de ventas

--- models/pos_order.py
+++ models/pos_order.py
@@ -58,6 +58,15 @@ class PosOrder(models.Model):
 
         return res
 
+    @api.model
+    def search_paid_order_ids(self, config_id, domain, limit, offset):
+        """Search for 'paid' orders that satisfy the given domain, limit and offset."""
+        default_domain = ['!', '|', ('state', '=', 'draft'), ('state', '=', 'cancelled')]
+        real_domain = AND([domain, default_domain])
+        ids = self.search(AND([domain, default_domain]), limit=limit, offset=offset).ids
+        totalCount = self.search_count(real_domain)
+        return {'ids': ids, 'totalCount': totalCount}
+
 class PosSession(models.Model):
     _inherit = 'pos.session'
 
