3869b1cd99662be44e57602bae083dc8aebb0677
jeffrepo <jeffer.leo93@gmail.com>
2024-08-13 07:36:32 -0600

    Generar nota de crédito

--- models/pos_order.py
+++ models/pos_order.py
@@ -8,6 +8,10 @@ import logging
 class PosOrder(models.Model):
     _inherit = 'pos.order'
 
+    pedido_origen_id = fields.Many2one('pos.order', string='Pedido Origen')
+    nota_credito_creada = fields.Boolean('Nota credito creada', default=False)
+    permitir_devolver = fields.Boolean('Permitir devolver', related='session_id.config_id.permitir_devolver')
+
     def _prepare_invoice_line(self, order_line):
         res = super(PosOrder, self)._prepare_invoice_line(order_line)
         if order_line.order_id.config_id.analytic_account_id:
@@ -32,6 +36,43 @@ class PosOrder(models.Model):
         ids = self.search(AND([domain, default_domain]), limit=limit, offset=offset).ids
         totalCount = self.search_count(real_domain)
         return {'ids': ids, 'totalCount': totalCount}
+    
+    def refund(self):
+        res = super(PosOrder, self).refund()
+        nuevo = self.browse(res['res_id'])
+        nuevo.pedido_origen_id = self
+
+        return res
+
+    def nota_credito(self):
+        if self.nota_credito_creada:
+            raise UserError('La nota de crédito ya ha sido creada para este pedido.')
+
+        res = self.refund()
+        nuevo = self.browse(res['res_id'])
+        for p in self.payment_ids:
+            nuevo.add_payment({
+                'name': _('return'),
+                'pos_order_id': nuevo.id,
+                'amount': -p.amount,
+                'payment_date': fields.Date.context_today(self),
+                'payment_method_id': p.payment_method_id.id,
+            })
+
+        nuevo.action_pos_order_paid()
+        nuevo._create_order_picking()
+        nuevo.action_pos_order_invoice()
+
+        nuevo.nota_credito_creada = True
+        self.nota_credito_creada = True
+
+        return res
+
+    def _prepare_invoice_vals(self):
+        res = super(PosOrder, self)._prepare_invoice_vals()
+        if res['move_type'] == "out_refund":
+            res['journal_id'] = self.session_id.config_id.diario_nota_credito_id.id
+        return res
 
 class PosSession(models.Model):
     _inherit = 'pos.session'
