fb063bc7234b9dc0c5c43ad608cd166fa62457f2
rborstgithub <56568136+rborstgithub@users.noreply.github.com>
2024-09-13 07:58:05 -0600

    Cambio default_pos_id a many2many

--- models/res_users.py
+++ models/res_users.py
@@ -5,6 +5,6 @@ from odoo import models, fields, api, _
 class Users(models.Model):
     _inherit = 'res.users'
 
-    default_pos_id = fields.Many2one("pos.config", string="Punto de Venta por Defecto")
+    default_pos_id = fields.Many2many("pos.config", string="Punto de Venta por Defecto")
     default_location_id = fields.Many2one(related="default_pos_id.picking_type_id.default_location_src_id", readonly=True)
     default_analytic_account_id = fields.Many2one(related="default_pos_id.analytic_account_id", readonly=True)
