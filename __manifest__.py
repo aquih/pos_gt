a18c43eb4e5b45246e66d5660dc4c997792fb185
Ariana Carrillo <ariana@computadora911.com>
2025-03-20 21:27:20 +0000

    quitar campo take_out en vista de pedidos

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
         'security/pos_gt_security.xml',
         'security/ir.model.access.csv',
         # 'views/pos_extra_view.xml',
-        'views/pos_order_view.xml',
+        #'views/pos_order_view.xml',
         # 'views/account_views.xml',
     ],
     'installable': True,
