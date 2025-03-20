566d7232264d8da3d9c915d26ee74d5511131942
Ariana Carrillo <ariana@computadora911.com>
2025-03-20 20:28:25 +0000

    Guardar campo take_out en el pedido

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Point of Sale para Guatemala',
-    'version': '5.0',
+    'version': '5.1',
     'category': 'Point of Sale',
     'sequence': 6,
     'summary': 'Cambios al Punto de Venta para el manejo en Guatemala',
