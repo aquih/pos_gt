7c8987d6414068b67f295577a16f0206669a4353
rocketgithub <rodrigo@aquih.com>
2025-05-26 21:31:54 -0600

    Otras vistas viejas a borrar

--- migrations/17.0.5.0/pre-borrar-vistas.py
+++ migrations/17.0.5.0/pre-borrar-vistas.py
@@ -6,4 +6,8 @@ _logger = logging.getLogger(__name__)
 
 def migrate(cr, version):
     util.records.remove_view(cr, xml_id="pos_gt.pos_gt_view_pos_pos_form")
+    util.records.remove_view(cr, xml_id="pos_gt.pos_gt_extra_tree_view")
+    util.records.remove_view(cr, xml_id="pos_gt.pos_gt_extra_form_view")
+    util.records.remove_view(cr, xml_id="pos_gt.pos_gt_invoice_form")
+    util.records.remove_view(cr, xml_id="pos_gt.res_users_form_view_gt")
     _logger.info("Vistas viejas borradas")
