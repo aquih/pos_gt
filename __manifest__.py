afaa3fec14d5780d70c0acdb04b64660717b81c3
rocketgithub <rodrigo@aquih.com>
2025-06-09 14:58:36 -0600

    Aumentar version

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Point of Sale para Guatemala',
-    'version': '5.2',
+    'version': '6.2',
     'category': 'Point of Sale',
     'sequence': 6,
     'summary': 'Cambios al Punto de Venta para el manejo en Guatemala',
