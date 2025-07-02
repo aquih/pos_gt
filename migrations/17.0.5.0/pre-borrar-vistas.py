eaa30f808847b198b8b7b38dadec46ef452d1173
rocketgithub <rodrigo@aquih.com>
2025-07-02 14:31:59 -0600

    Otra vista para borrar en la migración

--- migrations/17.0.5.0/pre-borrar-vistas.py
+++ migrations/17.0.5.0/pre-borrar-vistas.py
@@ -10,5 +10,6 @@ def migrate(cr, version):
     util.records.remove_view(cr, xml_id="pos_gt.pos_gt_extra_form_view")
     util.records.remove_view(cr, xml_id="pos_gt.pos_gt_invoice_form")
     util.records.remove_view(cr, xml_id="pos_gt.res_users_form_view_gt")
+    util.records.remove_view(cr, xml_id="pos_gt.pos_config_view_form_pos_gt")
     util.records.remove_view(cr, xml_id="pos_gt.product_template_form_view_pos_gt")
     _logger.info("Vistas viejas borradas")
