59d767fe6373976d85113c591a1500858c884701
rocketgithub <rodrigo@aquih.com>
2025-07-02 14:35:32 -0600

    Otra vista para borrar en la migración

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Point of Sale para Guatemala',
-    'version': '6.2',
+    'version': '6.3',
     'category': 'Point of Sale',
     'sequence': 6,
     'summary': 'Cambios al Punto de Venta para el manejo en Guatemala',
