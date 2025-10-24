7ffc1c60ea6d808ae62ca843dd89cecbbddacb71
rocketgithub <rodrigo@aquih.com>
2025-10-23 18:36:49 -0600

    Obtener compañía correcta

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Point of Sale para Guatemala',
-    'version': '5.3',
+    'version': '5.4',
     'category': 'Point of Sale',
     'sequence': 6,
     'summary': 'Cambios al Punto de Venta para el manejo en Guatemala',
--- report/reporte_cierre.py
+++ report/reporte_cierre.py
@@ -68,4 +68,5 @@ class ReporteCierre(models.AbstractModel):
             'total_ingresos': self.total_ingresos,
             'lineas_egresos': self.lineas_egresos,
             'total_egresos': self.total_egresos,
+            'current_company_id': self.env.company,
         }
