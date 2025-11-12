6e09de27f02586a846fc4cb8f14c59037cbbec00
Rodrigo Fernández <rodrigo@aquih.com>
2025-11-12 18:11:32 +0000

    Usar dirección completa para el ticket.

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Point of Sale para Guatemala',
-    'version': '6.5',
+    'version': '6.6',
     'category': 'Point of Sale',
     'sequence': 6,
     'summary': 'Cambios al Punto de Venta para el manejo en Guatemala',
