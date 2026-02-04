d7179d687dd5caa0877d9157f67b77c580d7800b
Rodrigo Fernández <rodrigo@aquih.com>
2026-02-04 21:55:38 +0000

    Version 19.0 ¯\_(ツ)_/¯

--- models/pos_order.py
+++ models/pos_order.py
@@ -9,7 +9,7 @@ class PosOrder(models.Model):
     _inherit = 'pos.order'
 
     def _get_invoice_lines_values(self, line_values, pos_line, move_type):
-        res = super(PosOrder, self)._get_invoice_lines_values(lline_values, pos_line, move_type)
+        res = super(PosOrder, self)._get_invoice_lines_values(line_values, pos_line, move_type)
         if pos_line.order_id.config_id.analytic_account_id:
             res['analytic_distribution'] = dict([(str(pos_line.order_id.config_id.analytic_account_id.id), 100),])
         return res
