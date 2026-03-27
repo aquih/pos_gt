9096f4caccfe12b4320e29b2436f8acd1534459a
rocketgithub <rodrigo@aquih.com>
2026-03-27 10:52:37 -0600

    Texto de ayuda correcto.

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Point of Sale para Guatemala',
-    'version': '6.8',
+    'version': '6.9',
     'category': 'Point of Sale',
     'sequence': 6,
     'summary': 'Cambios al Punto de Venta para el manejo en Guatemala',
